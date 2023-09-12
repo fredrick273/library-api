e5568dacb887a787bc8ba6ebd710d75ce5a5f20f
UnwantedSoul <91271714+Fredrick273@users.noreply.github.com>
2023-09-12 18:05:21 +0530

    added postgresql for database

--- backend/settings.py
+++ backend/settings.py
@@ -85,13 +85,28 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
+# UNCOMMENT THE FOLLOWING TO USE SQLITE
+
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'librarydb', # YOUR DATABASE
+        'USER': 'postgres',
+        'PASSWORD': 'admin',  # YOUR DATABASE PASSWORD
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
